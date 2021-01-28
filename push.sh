be6fb92697a447f43f9608b7ec4a3e90ffc48c5d
Andy Hablewitz <mail@andyhablewitz.com>
2021-01-27 22:20:56 -0700

    first push with basic actions scripts

--- push.sh
+++ push.sh
@@ -4,18 +4,18 @@ set -ev
 echo 'Updating gh-pages branch...';
 
 setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
+  git config --global user.email "ci@andyhablewitz.com"
+  git config --global user.name "CI/CD Pipeline"
 }
 
 commit_website_files() {
   git checkout -b gh-pages
   git add index.html
-  git commit --message "Updating resume HTML - Travis build: $TRAVIS_BUILD_NUMBER"
+  git commit --message "Updating resume HTML - ${{GITHUB_RUN_ID}}"
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/android2221/resume.git > /dev/null 2>&1
+  git remote add origin-pages https://${{GH_TOKEN}}@github.com/android2221/resume.git > /dev/null 2>&1
   git push --quiet --force --set-upstream origin-pages gh-pages 
 }
 
